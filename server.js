6a718347726f795a4999ce583cff26a32f3751f2
nitishmayekar <nitishmayekar333@gmail.com>
2023-09-24 19:31:03 -0400

    Final Commit

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const cors = require('cors');
 const express = require('express');
+const router = express.Router();
 const app = express();
+app.use(express.json());
+app.use(cors());
+const fs = require('fs');
 
+const videosDetailed = JSON.parse(
+    fs.readFileSync(`./assets/data/video-details.json`)
+  );
 
 app.get('/', (req, res) => {
-    // send some text back as a response
-    res.send('Express is running!');
+    res.status(200).json(videosDetailed);
 });
-// start Express on port 8080
-app.listen(8080, () => {
-    console.log('Server Started on http://localhost:8080');
-    console.log('Press CTRL + C to stop server');
+
+app.get('/videos/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const video = videosDetailed.find((el) => el.id === id);
+  res.status(200).json(video);
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
 });
